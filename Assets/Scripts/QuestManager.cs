ae8903b2585fc4a99c7621c479f141a3cf538d50
stopresent <86364202+stopresent@users.noreply.github.com>
2023-11-22 02:39:26 +0900

    [ADD] 2D 쯔꾸르 게임 모작 저장

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public int questId;
+    public int questActionIndex;
+    public GameObject[] questObject;
+
+    Dictionary<int, QuestData> questList;
+
+    void Awake()
+    {
+        questList = new Dictionary<int, QuestData>();
+        GenerateData();
+    }
+
+    // Update is called once per frame
+    void GenerateData()
+    {
+        questList.Add(10, new QuestData("마을 사람들과 대화하기"
+            , new int[] { 1000, 2000 }));
+        questList.Add(20, new QuestData("루도의 동전 찾아주기"
+            , new int[] { 5000, 2000 }));
+        questList.Add(30, new QuestData("퀘스트 올 클리어"
+            , new int[] { 0 }));
+    }
+
+    public int GetQuestTalkIndex(int id)
+    {
+        return questId + questActionIndex;
+    }
+
+    public string CheckQuest(int id)
+    {
+        // Next Talk Target
+        if (id == questList[questId].npcId[questActionIndex])
+            questActionIndex++;
+
+        // Control Quest Object
+        ControlObject();
+
+        // Talk Complete & Next Quest
+        if (questActionIndex == questList[questId].npcId.Length)
+            NextQuest();
+
+        // Quest Name
+        return questList[questId].questName;
+    }
+
+    public string CheckQuest()
+    {
+        // Quest Name
+        return questList[questId].questName;
+    }
+
+    void NextQuest()
+    {
+        questId += 10;
+        questActionIndex = 0;
+    }
+
+    public void ControlObject()
+    {
+        switch (questId)
+        {
+            case 10:
+                if (questActionIndex == 2)
+                    questObject[0].SetActive(true);
+                break;
+            case 20:
+                if (questActionIndex == 0)
+                    questObject[0].SetActive(true);
+                else if (questActionIndex == 1)
+                    questObject[0].SetActive(false);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+    Dictionary<int, Sprite> portraitData;
+
+    public Sprite[] portraitArr;
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>();
+        GenerateData();
+    }
+
+    void GenerateData()
+    {
+        // Talk Data
+        // NPC A : 1000, NPC B : 2000
+        // Box: 3000, Desk: 4000
+        talkData.Add(1000, new string[] { "안녕?:0"
+            , "반가워:1" });
+        talkData.Add(2000, new string[] { "뭐야?:0"
+            , "할 말이 있는거야?:1"
+            , "없으면 말구:2" });
+
+        talkData.Add(3000, new string[] { "데스크엔 이런 말이 있다."
+            , "이곳이 다 막혀있다고 생각하는 가?"
+            , "새 길을 찾아 따라 가다보면 값진 무언가를 얻을 수 있을 것이다." });
+        talkData.Add(4000, new string[] { "상자 속에 냅킨이 하나 있다."
+            , "010 - **** - ****\n친구 닮은 사람이예요 ^^"
+            , "왠지 모를 옅은 미소가 새어 나왔다." });
+
+        // Quest Talk
+        talkData.Add(10 + 1000, new string[] { "어서와:0"
+            ,"이 마을에 놀라운 전설이 있다는데:1"
+            ,"오른쪽에 있는 루도가 알려줄거야:0" });
+        talkData.Add(11 + 2000, new string[] { "여어:1"
+            , "이 호수의 전설을 들으러 온거야?:0"
+            , "그럼 일 좀 하나 해주면 좋을텐데...:1"
+            , "이 곳 어딘가에 있는 떨어진 동전 좀 주워줬으면 좋겠어.:2" });
+
+        talkData.Add(20 + 1000, new string[] { "루도의 동전?:1"
+            ,"돈을 흘리고 다니면 못쓰지!:3"
+            ,"나중에 루도에게 한마디 해야겠어.:3"});
+        talkData.Add(20 + 2000, new string[] { "찾으면 꼭 좀 가져다 줘:1" });
+        talkData.Add(20 + 5000, new string[] { "동전을 찾았다"
+            ,"이런곳에 있었군.."});
+        talkData.Add(21 + 2000, new string[] { "엇, 찾아줘서 고마워.:2" });
+
+        // Portrait Data
+        // 0: Normal, 1: Speak, 2: Happy, 3: Angry
+        portraitData.Add(1000 + 0, portraitArr[0]);
+        portraitData.Add(1000 + 1, portraitArr[1]);
+        portraitData.Add(1000 + 2, portraitArr[2]);
+        portraitData.Add(1000 + 3, portraitArr[3]);
+        portraitData.Add(2000 + 0, portraitArr[4]);
+        portraitData.Add(2000 + 1, portraitArr[5]);
+        portraitData.Add(2000 + 2, portraitArr[6]);
+        portraitData.Add(2000 + 3, portraitArr[7]);
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (!talkData.ContainsKey(id))
+        {
+            if (!talkData.ContainsKey(id - id % 10))
+                return GetTalk(id - id % 100, talkIndex); // Get First Talk            
+            else
+                return GetTalk(id - id % 10, talkIndex); // Get First Quest Talk
+        }
+
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+
+    public Sprite GetPortrait(int id, int portraitIndex)
+    {
+        return portraitData[id + portraitIndex];
+    }
+}
